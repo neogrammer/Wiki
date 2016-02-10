6813c44143484879d976050b15c050e6bfca2794
Chuck Walbourn <chuckw@windows.microsoft.com>
2016-02-09 16:45:57 -0800

    Updated ReadData for UWP/Xbox/PC use

--- ReadData.h
+++ ReadData.h
@@ -1,8 +1,9 @@
 //
 // ReadData.h
+//
 // Helper for loading binary data files from disk
 //
-// Looks for all files in the same folder as the running EXE
+// For Windows desktop apps, it looks for files in the same folder as the running EXE if it can't find them in the CWD
 //
 
 #include <stdint.h>
@@ -12,22 +13,30 @@
 
 namespace DX
 {
-    inline std::vector<uint8_t> ReadData(const wchar_t* name)
+    inline std::vector<uint8_t> ReadData(_In_z_ const wchar_t* name)
     {
-        wchar_t moduleName[_MAX_PATH];
-        (void) GetModuleFileNameW(nullptr, moduleName, _MAX_PATH);
+        std::ifstream inFile(name, std::ios::in | std::ios::binary | std::ios::ate);
 
-        wchar_t drive[_MAX_DRIVE];
-        wchar_t path[_MAX_PATH];
+#if !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP)
+        if (!inFile)
+        {
+            wchar_t moduleName[_MAX_PATH];
+            (void)GetModuleFileNameW(nullptr, moduleName, _MAX_PATH);
 
-        if (_wsplitpath_s(moduleName, drive, _MAX_DRIVE, path, _MAX_PATH, nullptr, 0, nullptr, 0))
-            throw std::exception("_wsplitpath_s");
+            wchar_t drive[_MAX_DRIVE];
+            wchar_t path[_MAX_PATH];
 
-        wchar_t filename[_MAX_PATH];
-        if (_wmakepath_s(filename, _MAX_PATH, drive, path, name, nullptr))
-            throw std::exception("_wmakepath_s");
+            if (_wsplitpath_s(moduleName, drive, _MAX_DRIVE, path, _MAX_PATH, nullptr, 0, nullptr, 0))
+                throw std::exception("_wsplitpath_s");
 
-        std::ifstream inFile(filename, std::ios::in | std::ios::binary | std::ios::ate);
+            wchar_t filename[_MAX_PATH];
+            if (_wmakepath_s(filename, _MAX_PATH, drive, path, name, nullptr))
+                throw std::exception("_wmakepath_s");
+
+            inFile.open(filename, std::ios::in | std::ios::binary | std::ios::ate);
+        }
+#endif
+
         if (!inFile)
             throw std::exception("ReadData");
 
